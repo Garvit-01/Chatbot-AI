6bcbd5a294d29d45240aff40ecf4f74bb4b86674
Garvit-01 <garvitkatyal2@gmail.com>
2021-07-09 15:27:08 +0530

    Done

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ import pickle
 import json
 
 import nltk
-nltk.download('punkt')
-nltk.download('wordnet')
 
 from nltk.stem import WordNetLemmatizer
 from tensorflow.keras.models import Sequential
@@ -44,7 +42,8 @@ classes = sorted(set(classes))
 
 # to saving the words
 pickle.dump(words, open('words.pkl', 'wb'))
-pickle.dump(words, open('classes.pkl', 'wb'))
+
+pickle.dump(classes, open('classes.pkl', 'wb'))
 
 # Neutral network needs numerical value and not words
 # so we are going to use a concept of bag of words
@@ -58,7 +57,7 @@ output_empty = [0] * len(classes)
 for document in documents:
     bag = []
     word_patterns = document[0]
-    word_patterns = [ lemmatizer.lemmatize(word.lower()) for word in word_patterns]
+    word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
     for word in words:
         bag.append(1) if word in word_patterns else bag.append(0)
 
@@ -68,7 +67,7 @@ for document in documents:
     training.append([bag, output_row])
 
 random.shuffle(training)
-training =np.array(training)
+training = np.array(training)
 
 train_x = list(training[:, 0])
 train_y = list(training[:, 1])
@@ -83,8 +82,8 @@ model.add(Dropout(0.5))
 model.add(Dense(len(train_y[0]), activation='softmax'))
 
 sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-model.compile(loss='categorical crossentrophy', optimizer=sgd, metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
-model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
-model.save('chatbox_model.model')
+hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+model.save('chatbotmodel.h5', hist)
 print("Done")
